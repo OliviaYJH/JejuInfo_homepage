fdd8fcac78d0a819d7d2df860c10039bc39a3f51
Yu-Jung-Hyun <81751105+Yu-Jung-Hyun@users.noreply.github.com>
2021-06-25 00:08:52 +0900

    Add files via upload

---
+++ JEJUINFO.js
@@ -0,0 +1,22 @@
+var pic1 = document.getElementById("img1");
+var pic2 = document.getElementById("img2");
+
+function big1(){
+    pic1.style.width = "300px";
+    pic1.style.height = "300px";
+}
+
+function small1(){
+    pic1.style.width = "250px";
+    pic1.style.height = "250px";
+}
+
+function big2(){
+    pic2.style.width = "300px";
+    pic2.style.height = "300px";
+}
+
+function small2(){
+    pic2.style.width = "250px";
+    pic2.style.height = "250px";
+}
---
+++ Notice.js
@@ -0,0 +1,56 @@
+// 내용 자세히 보기 및 닫기
+function display1(){
+    var dom = document.getElementById("no1");
+    var button = document.getElementById("no1button");
+
+    if(dom.style.display == 'none'){
+        dom.style.display = 'block';
+        button.value="닫기";
+    }
+    else{
+        dom.style.display = 'none';
+        button.value="자세히보기";
+    }
+}
+
+function display2(){
+    var dom = document.getElementById("no2");
+    var button = document.getElementById("no2button");
+
+    if(dom.style.display == 'none'){
+        dom.style.display = 'block';
+        button.value="닫기";
+    }
+    else{
+        dom.style.display = 'none';
+        button.value="자세히보기";
+    }
+}
+
+function display3(){
+    var dom = document.getElementById("no3");
+    var button = document.getElementById("no3button");
+
+    if(dom.style.display == 'none'){
+        dom.style.display = 'block';
+        button.value="닫기";
+    }
+    else{
+        dom.style.display = 'none';
+        button.value="자세히보기";
+    }
+}
+
+function display4(){
+    var dom = document.getElementById("no4");
+    var button = document.getElementById("no4button");
+
+    if(dom.style.display == 'none'){
+        dom.style.display = 'block';
+        button.value="닫기";
+    }
+    else{
+        dom.style.display = 'none';
+        button.value="자세히보기";
+    }
+}
